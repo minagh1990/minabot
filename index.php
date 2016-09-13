73cedbc4bd71f24dfa451d43dd96798d3608ba70
minagh1990 <mini.gh1990@yahoo.com>
2016-09-13 20:11:47 +0430

    Update index.php
    edit index.php

--- index.php
+++ index.php
@@ -2,23 +2,21 @@
 set_time_limit(60);
 date_default_timezone_set('Asia/Tehran');
 header('Content-type: application/json');
-
-
     $update = json_decode(file_get_contents('php://input'), true);
-
     $message = $update["message"];
     $telegram_id = $message['from']['id'];
     $userInput = $message['text'];
-    $replyText = 'سلام';
+    $replyText = 'سلام به بازی حدس اعداد خوش آمدید\nلطفا یک عدد از بین 1تا20انتخاب کنید و کامنت yes را انتخاب کنید';
     $reply = [
         'method' => 'sendMessage',
         'chat_id' => $message['chat']['id'],
         'text' => $replyText,
     ];
-
+	if($message['chat']['id']=="yes"){
+		$replyText='آیا عدد شما کمتر از 10است؟'
+	}
+	else{
+		$replyText ='سلام'
+	}
     echo json_encode($reply);
-
-
-
-
-
+?>
