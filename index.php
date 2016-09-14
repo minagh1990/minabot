377e59a6404bbe70b501e5f342f1d183bfebe437
minagh1990 <mini.gh1990@yahoo.com>
2016-09-14 21:40:21 +0430

    Update index.php
    edit

--- index.php
+++ index.php
@@ -6,7 +6,6 @@ header('Content-type: application/json');
     $message = $update["message"];
     $telegram_id = $message['from']['id'];
     $userInput = $message['text'];
-    $numberYes=1;
 
     if( $userInput=="/start"){
     $replyText = 'سلام به بازی حدس اعداد خوش آمدید.لطفا یک عدد از بین یک تا هزار انتخاب کنید و yesرا بزنید';
@@ -26,7 +25,8 @@ header('Content-type: application/json');
         'text' => $replyText2,
     ];
      echo json_encode($reply2);
-     if($userInput=="/no"){
+}
+     if($replyText2=="آیا عدد انتخابی شما از پانصد بیشتر است؟" && $userInput=="/no"){
       $replyText4='آیا عدد انتخابی بیشتر از هشتصد است؟';
           $reply4 = [
         'method' => 'sendMessage',
@@ -35,4 +35,12 @@ header('Content-type: application/json');
     ];
      echo json_encode($reply4); 
 }
-}
+ if($replyText2=="آیا عدد انتخابی شما از پانصد بیشتر است؟" && $userInput == "/yes") {
+     $replyText3='آیا عدد انتخابی شما از دویست و پنجاه بیشتر است؟';
+    $reply3 = [
+        'method' => 'sendMessage',
+        'chat_id' => $message['chat']['id'],
+        'text' => $replyText3,
+    ];
+     echo json_encode($reply3);
+ }
