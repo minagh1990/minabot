6acb0067652b028fe13d4c81ff7cd7c72fd70c0e
minagh1990 <mini.gh1990@yahoo.com>
2016-09-14 22:12:46 +0430

    Update index.php
    edit

--- index.php
+++ index.php
@@ -25,6 +25,23 @@ header('Content-type: application/json');
         'text' => $replyText2,
     ];
      echo json_encode($reply2);
+      if($userInput=="/yes"){
+        $replyText4='آیا عدد انتخابی شما از هشتصد بیشتر است؟';
+        $reply4 = [
+            'method' => 'sendMessage',
+            'chat_id' => $message['chat']['id'],
+            'text' => $replyText4,
+        ];
+        echo json_encode($reply4);  
+    }elseif ($userInput=="/no"){
+        $replyText5='آیا عدد انتخابی شما از دویست و پنجاه بیشتر است؟';
+        $reply5 = [
+            'method' => 'sendMessage',
+            'chat_id' => $message['chat']['id'],
+            'text' => $replyText5,
+        ];
+        echo json_encode($reply5);
+    }
 }
    else  if($userInput=="/no"){
       $replyText3 = 'سلام به بازی حدس اعداد خوش آمدید.لطفا یک عدد از بین یک تا هزار انتخاب کنید و yesرا بزنید';
