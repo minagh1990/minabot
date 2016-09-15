68fc4c6b0ae98f04e2237ea649e6c724e27aebdd
alireza <azerafati@gmail.com>
2016-09-15 14:03:05 +0430

    fixing getting update

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 set_time_limit(60);
 date_default_timezone_set('Asia/Tehran');
 header('Content-type: application/json');
-$update = $_req;
+$update = json_decode(file_get_contents('php://input'),true);
 $message = $update["message"];
 $telegram_id = $message['from']['id'];
 $userInput = $message['text'];
