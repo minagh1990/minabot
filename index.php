3d33ae37f1b759146a1ef666a3420065e4a4ec6f
minagh1990 <mini.gh1990@yahoo.com>
2016-09-15 13:38:25 +0430

    Update index.php
    edit

--- index.php
+++ index.php
@@ -25,31 +25,10 @@ header('Content-type: application/json');
         'text' => $replyText2,
     ];
      echo json_encode($reply2);
-      if($userInput=="/yes"){
-        $replyText4='آیا عدد انتخابی شما از هشتصد بیشتر است؟';
-        $reply4 = [
-            'method' => 'sendMessage',
-            'chat_id' => $message['chat']['id'],
-            'text' => $replyText4,
-        ];
-        echo json_encode($reply4);  
-    }elseif ($userInput=="/no"){
-        $replyText5='آیا عدد انتخابی شما از دویست و پنجاه بیشتر است؟';
-        $reply5 = [
-            'method' => 'sendMessage',
-            'chat_id' => $message['chat']['id'],
-            'text' => $replyText5,
-        ];
-        echo json_encode($reply5);
-    }
+     InlineKeyboardButton[][] buttons1 = new InlineKeyboardButton[2][1];
+buttons1[0][0] = new InlineKeyboardButton("Yes", null, "آیا عدد انتخابی شما از هفتصدوپنجاه بیشتر است؟", null);
+buttons1[0][1] = new InlineKeyboardButton("No", null, "آیا عدد انتخابی شما از دویست و پنجاه بیشتر است؟", null);
+
 }
-   else  if($userInput=="/no"){
-      $replyText3 = 'سلام به بازی حدس اعداد خوش آمدید.لطفا یک عدد از بین یک تا هزار انتخاب کنید و yesرا بزنید';
-    $reply3 = [
-        'method' => 'sendMessage',
-        'chat_id' => $message['chat']['id'],
-        'text' => $replyText,
-    ];
-    echo json_encode($reply3);
-}
+
  
